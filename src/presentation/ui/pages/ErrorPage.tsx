c0e0f4cd8edbdc7fe27afce28ad5e67b344d46fb
samir <bettahar.samir@outlook.com>
2023-12-05 09:57:39 +0100

    updating error page

--- src/presentation/ui/pages/ErrorPage.tsx
+++ src/presentation/ui/pages/ErrorPage.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
-  return <h1>Something went wrong</h1>;
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Something went wrong</h1>
+      <Link to="../login/">Login again</Link>
+    </>
+  );
 }
