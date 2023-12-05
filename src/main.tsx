8e237f36d6591885e04f70104f18d58e12a317fa
samir <bettahar.samir@outlook.com>
2023-12-05 09:46:53 +0100

    adding a parent suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import AdminContextProvider from "./app/context/AdminContext.tsx";
 import { router } from "./app/router/router.tsx";
+import Spinner from "./presentation/ui/components/Spinner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AdminContextProvider>
-      <RouterProvider router={router} />
-    </AdminContextProvider>
+    <Suspense fallback={<Spinner />}>
+      <AdminContextProvider>
+        <RouterProvider router={router} />
+      </AdminContextProvider>
+    </Suspense>
   </React.StrictMode>
 );
